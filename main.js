57b80a1623c63305bdf76f28f57c7ae3d370c9ad
CristhinnySarah <cristhinnysarah@gmail.com>
2022-12-04 09:39:07 -0300

    Falta arrumar EOF e espaços

--- main.js
+++ main.js
@@ -46,8 +46,7 @@ function isAlfabeto(caractere) {
 
 let linha = 1
 let coluna = 0
-let cabecote
-let palavra = ""
+let cabecote = 0
 
 let tabelaDeSimbolos = [
 	{classeToken: 'inicio', tipoToken: 'inicio', lexemaToken: 'inicio'},
@@ -67,9 +66,20 @@ let tabelaDeSimbolos = [
 // Vamos implementar o automato criado pelo Julio
 const maquinaDeterministica = {
   estado: 0,
+  token: {
+    classeToken: '', tipoToken: '', lexemaToken: ''
+  },
   transitions: {
     0: {
       readCharacter: function(data){
+        // fazer EOF funcionar!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        if(data.caractere == 0){
+          this.token.classeToken = 'EOF';
+          this.token.tipoToken = 'NULO';
+          return this.token;
+          //this.changeState(17);
+        }
+        this.token.lexemaToken = data.caractere;
         if(isAlfabeto(data.caractere)){
           if(isLetra(data.caractere)){
             this.changeState(1);
@@ -88,7 +98,8 @@ const maquinaDeterministica = {
           } else if(data.caractere == ','){
             this.changeState(10); 
           } else if(isDigito(data.caractere)){
-            this.changeState(11); 
+            this.changeState(11);
+            this.token.tipoToken = 'INTEIRO'; 
           } else if(data.caractere == '('){
             this.changeState(18); 
           } else if(data.caractere == ')'){
@@ -96,234 +107,296 @@ const maquinaDeterministica = {
           } else if(data.caractere == '{'){
             this.changeState(20); 
           } else if(isCaractereDeQuebra(data.caractere)){
-            return -2;
-          } else if(data.caractere == 0){
-            this.changeState(17);
-          } 
-          return -1;
+            console.log("Ignorando Espaço")
+            this.token.lexemaToken = '';
+            this.token.classeToken = 'IGNORAR';
+            this.token.tipoToken = 'NULO';
+            return this.token;
+          }  
         }
         // erro
-        else return 22;
+        else{
+          console.log("ERRO LÉXICO - Caractere inválido na linguagem. Linha " + linha + ", coluna " + coluna)
+          this.token.classeToken = 'ERROR';
+          this.token.tipoToken = 'NULO';
+          return this.token; 
+        } 
       }
     },
     1: {
       readCharacter: function(data){
-        if(isLetra(data.caractere) || isDigito(data.caractere) || data.caractere == '_') return -1;
+        if(isLetra(data.caractere) || isDigito(data.caractere) || data.caractere == '_'){
+          this.token.lexemaToken = this.token.lexemaToken + data.caractere;
+          return null;
+        } 
         else{
           this.changeState(0)
-          return 1;  
+          this.token.classeToken = 'ID';
+          this.token.tipoToken = 'NULO';
+          return this.token;  
         }
       }
     },
     2:{
       readCharacter: function(data){
         if(data.caractere == '-'){
+          this.token.lexemaToken = this.token.lexemaToken + data.caractere;
           this.changeState(4)
-          return -1;
+          return null;
         }
         if(data.caractere == '=' || data.caractere == '>'){
+          this.token.lexemaToken = this.token.lexemaToken + data.caractere;
           this.changeState(3)
-          return -1;
+          return null;
+        }
+        else{
+          this.changeState(0)
+          this.token.classeToken = 'OPR';
+          this.token.tipoToken = 'NULO';
+          return this.token;
         }
       }
     },
     3:{
       readCharacter: function(data){
         this.changeState(0)
-        return 3;
+        this.token.classeToken = 'OPR';
+        this.token.tipoToken = 'NULO';
+        return this.token;
       }
     },
     4:{
       readCharacter: function(data){
         this.changeState(0)
-        return 4;
+        this.token.classeToken = 'ATR';
+        this.token.tipoToken = 'NULO';
+        return this.token;
       }
     },
     5:{
       readCharacter: function(data){
         if(data.caractere == '='){
+          this.token.lexemaToken = this.token.lexemaToken + data.caractere;
           this.changeState(3)
-          return -1;
+          return null;
         }
-        else{
+        else {
           this.changeState(0)
-          return 5;
+          this.token.classeToken = 'OPR';
+          this.token.tipoToken = 'NULO';
+          return this.token;
         }
+        
       }
     },
     6: {
       readCharacter: function(data){
         this.changeState(0)
-        return 6;
+        this.token.classeToken = 'OPA';
+        this.token.tipoToken = 'NULO';
+        return this.token;
       }
     },
     7:{
       readCharacter: function(data){
+        this.token.lexemaToken = this.token.lexemaToken + data.caractere;
         if(data.caractere == '\"'){
           this.changeState(8)
-          return -1;
+          return null;
         }
-        else return -1; 
+        else return null; 
       }
     },
     8:{
       readCharacter: function(data){
         this.changeState(0)
-        return 8;
+        this.token.classeToken = 'LIT';
+        this.token.tipoToken = 'LITERAL';
+        return this.token;
       }
     },
     9:{
       readCharacter: function(data){
         this.changeState(0)
-        return 9;
+        this.token.classeToken = 'PT_V';
+        this.token.tipoToken = 'NULO';
+        return this.token;
       }
     },
     10:{
       readCharacter: function(data){
         this.changeState(0)
-        return 10;
+        this.token.classeToken = 'VIR';
+        this.token.tipoToken = 'NULO';
+        return this.token;
       }
     },
     11: {
       readCharacter: function(data){
-        if(isAlfabeto(data.caractere)){
-          if(isDigito(data.caractere)) return -1;
-          if(isCaractereDeQuebra(data.caractere) || data.caractere == 0){
-            this.changeState(0)
-            return 11;
-          }
+        if(isDigito(data.caractere)){
+          this.token.lexemaToken = this.token.lexemaToken + data.caractere;
+          return null;
+        } 
+        else{
           if(data.caractere == "."){
+            this.token.lexemaToken = this.token.lexemaToken + data.caractere;
+            this.token.tipoToken = 'REAL';
             this.changeState(12)
-            return -1; 
+            return null; 
           }
           if(data.caractere == "e" || data.caractere == "E"){
+            this.token.lexemaToken = this.token.lexemaToken + data.caractere;
             this.changeState(14)
-            return -1;
+            return null;
           }
-        }
-        else{
-          this.changeState(0)
-          return 11;
-        }
+          else{
+            this.changeState(0)
+            this.token.classeToken = 'NUM';
+            return this.token;
+          }
+      }
       }
     },
     12:{
       readCharacter: function(data){
+        this.token.lexemaToken = this.token.lexemaToken + data.caractere;
         if(isDigito(data.caractere)){
           this.changeState(13)
-          return -1;
+          return null;
         }
         else{
-          this.changeState(22)
-          return -1;
+          console.log("ERRO LÉXICO – Palavra não pertence à linguagem")
+          this.changeState(0);
+          this.token.classeToken = 'ERROR';
+          this.token.tipoToken = 'NULO';
+          return this.token;
         }    
       }
     },
     13:{
       readCharacter: function(data){
-        if(isAlfabeto(data.caractere)){
-          if(isDigito(data.caractere)) return -1;
+        this.token.lexemaToken = this.token.lexemaToken + data.caractere;
+        if(isDigito(data.caractere)) return null;
+        else{
           if(data.caractere == "e" || data.caractere == "E"){
             this.changeState(14)
-            return -1;
+            return null;
           }
-        }
-        else{
-          this.changeState(0)
-          return 13;
+          else{
+            this.changeState(0)
+            this.token.classeToken = 'NUM';
+            return this.token;
+          }
         }
       }
     },
     14:{
       readCharacter: function(data){
-        if(isAlfabeto(data.caractere)){
-          if(isDigito(data.caractere)){
-            this.changeState(15)
-            return -1;
-          }
-          if(data.caractere == "+" || data.caractere == "-"){
-            this.changeState(16)
-            return -1;
-          }
+        this.token.lexemaToken = this.token.lexemaToken + data.caractere;
+        if(isDigito(data.caractere)){
+          this.changeState(15)
+          return null;
         }
         else{
-          this.changeState(0)
-          return 22;
+          if(data.caractere == "+" || data.caractere == "-"){
+            this.changeState(16)
+            return null;
+          }
+          else{
+            console.log("ERRO LÉXICO – Palavra não pertence à linguagem")
+            this.changeState(0);
+            this.token.classeToken = 'ERROR';
+            this.token.tipoToken = 'NULO';
+            return this.token;
+          }
         }
       }
     },
     15:{
       readCharacter: function(data){
-        if(isAlfabeto(data.caractere)){
-          if(isDigito(data.caractere)) return -1;
+        this.token.lexemaToken = this.token.lexemaToken + data.caractere;
+        if(isDigito(data.caractere)) return null;
+        else{
           if(data.caractere == "+" || data.caractere == "-"){
             this.changeState(16)
-            return -1;
+            return null;
+          }
+          else{
+            this.changeState(0)
+            this.token.classeToken = 'NUM';
+            return this.token;
           }
         }
-        else{
-          this.changeState(0)
-          return 15;
-        }
+        
       }
     },
     16:{
       readCharacter: function(data){
-        if(isAlfabeto(data.caractere)){
-          if(isDigito(data.caractere)){
-            this.changeState(15)
-            return -1;
-          }
+        this.token.lexemaToken = this.token.lexemaToken + data.caractere;
+        if(isDigito(data.caractere)){
+          this.changeState(15)
+          return null;
         }
         else{
-          this.changeState(0)
-          return 22;
+          console.log("ERRO LÉXICO – Palavra não pertence à linguagem")
+          this.changeState(0);
+          this.token.classeToken = 'ERROR';
+          this.token.tipoToken = 'NULO';
+          return this.token;
         }
       }
     },
-    17: {
+    17:{
       readCharacter: function(data){
         this.changeState(0)
-        return 17;
+        this.token.classeToken = 'EOF';
+        this.token.tipoToken = 'NULO';
+        return this.token;
       }
     },
     18:{
       readCharacter: function(data){
         this.changeState(0)
-        return 18;
+        this.token.classeToken = 'AB_P';
+        this.token.tipoToken = 'NULO';
+        return this.token;
       }
     },
     19:{
       readCharacter: function(data){
         this.changeState(0)
-        return 19;
+        this.token.classeToken = 'FC_P';
+        this.token.tipoToken = 'NULO';
+        return this.token;
       }
     },
     20:{
       readCharacter: function(data){
-        if(isAlfabeto(data.caractere)){
-          if(data.caractere == '}'){
-            this.changeState(21);
-            return -1;
+        this.token.lexemaToken = this.token.lexemaToken + data.caractere;
+        if(data.caractere == '}'){
+          this.changeState(21);
+          return null;
+        }
+        else {
+          if(data.caractere != 0 || isCaractereDeQuebra(data.caractere)){
+            return null;
           }
-          if(data.caractere == 0){
+          else {
             console.log("ERRO LÉXICO – Comentário não foi fechado, linha " + linha + ", coluna " + coluna)
             this.changeState(0);
-            return 22;
-          }
-          else return -1;
-        }
-        else{
-          console.log("ERRO LÉXICO – Caractere inválido na linguagem, linha " + linha + ", coluna " + coluna)
-            this.changeState(0);
-            return 22;
+            this.token.classeToken = 'ERROR';
+            this.token.tipoToken = 'NULO';
+            return this.token;
+          } 
         } 
       }
     },
     21:{
       readCharacter: function(data){
-        this.changeState(0);
-        return 21; 
+        this.changeState(0)
+        this.token.classeToken = 'COMENTÁRIO';
+        this.token.tipoToken = 'NULO';
+        return this.token;
       }
     }
   },
@@ -334,183 +407,56 @@ const maquinaDeterministica = {
     
     if(action){
       result = action.apply(maquinaDeterministica, ...payload);
-      console.log("Estado atual: " + this.estado + "\n")
-      if(result === 1){
-        return {
-          classeToken: 'ID',
-          tipoToken: 'NULO',
-          lexemaToken: ""
-        }
-      }
-      if(result === 2){
-        return {
-          classeToken: 'OPR',
-          tipoToken: 'NULO',
-          lexemaToken: ""
-        }
-      }
-      if(result === 3){
-        return {
-          classeToken: 'OPR',
-          tipoToken: 'NULO',
-          lexemaToken: ""
-        }
-      }
-      if(result === 4){
-        return {
-          classeToken: 'ATR',
-          tipoToken: 'NULO',
-          lexemaToken: ""
-        }
-      }
-      if(result === 5){
-        return {
-          classeToken: 'OPR',
-          tipoToken: 'NULO',
-          lexemaToken: ""
-        }
-      }
-      if(result === 6){
-        return {
-          classeToken: 'OPA',
-          tipoToken: 'NULO',
-          lexemaToken: ""
-        }
-      }
-      if(result === 8){
-        return {
-          classeToken: 'LIT',
-          tipoToken: 'literal',
-          lexemaToken: ""
-        }
-      }
-      if(result === 9){
-        return {
-          classeToken: 'PT_V',
-          tipoToken: 'NULO',
-          lexemaToken: ""
-        }
-      }
-      if(result === 10){
-        return {
-          classeToken: 'VIR',
-          tipoToken: 'NULO',
-          lexemaToken: ""
-        }
-      }
-      if(result === 11){
-        return {
-          classeToken: 'NUM',
-          tipoToken: 'inteiro',
-          lexemaToken: ""
-        }
-      }
-      if(result === 13){
-        return {
-          classeToken: 'NUM',
-          tipoToken: 'real',
-          lexemaToken: ""
-        }
-      }
-      if(result === 15){
-        return {
-          classeToken: 'NUM',
-          tipoToken: 'inteiro',
-          lexemaToken: ""
-        }
-      }
-      if(result === 17){
-        return {
-          classeToken: 'EOF',
-          tipoToken: 'NULO',
-          lexemaToken: ""
-        }
-      }
-      if(result === 18){
-        return {
-          classeToken: 'AB_P',
-          tipoToken: 'NULO',
-          lexemaToken: ""
-        }
-      }
-      if(result === 19){
-        return {
-          classeToken: 'FC_P',
-          tipoToken: 'NULO',
-          lexemaToken: ""
-        }
-      }
-      if(result === 21){
-        return {
-          classeToken: 'COMENTÁRIO',
-          tipoToken: 'NULO',
-          lexemaToken: ""
-        }
-      }
-      if(result == 22){
-        return {
-          classeToken: 'ERROR',
-          tipoToken: 'NULO',
-          lexemaToken: ""
-        }
-      }
-      if(result == -2){
-        return {
-          classeToken: 'IGNORAR',
-          tipoToken: 'NULO',
-          lexemaToken: ""
-        }
-      }
-      else{
-        return null
-      }
-    }else{
+      //console.log("Estado atual: " + this.estado)
+      
+      return result
+    } else {
       //action is not valid for current state
     }
   },
   changeState(novoEstado){
     this.estado = novoEstado;
+  },
+  resetToken(){
+    this.token.classeToken = '';
+    this.token.tipoToken = '';
+    this.token.lexemaToken = '';
   }
 }
 
 function SCANNER(data){
   let maquina = Object.create(maquinaDeterministica)
   let token
-  palavra = ""
-  //console.log("MAQUINA PRIMEIRO ESTADO: " + maquina.estado);
+  
+  //console.log("\nMAQUINA PRIMEIRO ESTADO: " + maquina.estado);
 
-  for(let i = cabecote; i < data.length; i++){
-    //console.log("Passo:|" + i + "| Estado:|" + maquina.estado + '| LINHA:|' + linha + '| COLUNA:|' + coluna + "| Lexema:|" + palavra + "|")
-    updateLinhaEColuna(data[i]);
-    
-    token = maquina.dispatch("readCharacter", [{caractere: data[i]}]);
-    
-    if(token == null) palavra = palavra + data[i]
+  for(let i = cabecote; i < data.length + 1; i++){
+    ///////////////////////////////////////////////////////////////////////////
+    if(i == data.length) token = maquina.dispatch("readCharacter", [{caractere: 0}]);
     else{
+      updateLinhaEColuna(data[i]); 
+      token = maquina.dispatch("readCharacter", [{caractere: data[i]}]);
+    } 
+    //////////////////////////////////////////////////////////////////////////
+    if(token == null)continue
+    else {
       if(token.classeToken != 'IGNORAR'){
-        token.lexemaToken = token.lexemaToken + palavra
         cabecote = i
-        
         //verificar se está na tabela de simbolos
         if(token.classeToken == 'ID' && SEARCH(token)) token = UPDATE(token.lexemaToken)
-  
         return token
       }
-      else cabecote = i+1
+      else {
+        console.log("Ignorando Espaço")
+        cabecote = i+1
+      }
     }
   }
   
   function updateLinhaEColuna(caractere){
     codigoASCII = caractere.charCodeAt(0);
-    /*
-    if(codigoASCII == '13')
-      console.log('Caractere:|' + '<-' + '| ASCII:|' + codigoASCII + '| LINHA:|' + linha + '| COLUNA:|' + coluna + '|');
-    else if(codigoASCII == '10')
-      console.log('Caractere:|' + '\\n' + '| ASCII:|' + codigoASCII + '| LINHA:|' + linha + '| COLUNA:|' + coluna + '|');
-    else
-      console.log('Caractere:|' + caractere + '| ASCII:|' + codigoASCII + '| LINHA:|' + linha + '| COLUNA:|' + coluna + '|');
-    */
     coluna++;
+    
     if(codigoASCII == '10'){
       linha++;
       coluna = 0;
@@ -547,18 +493,15 @@ function UPDATE(){
 function main(){
   const fs = require('fs')
   const data = fs.readFileSync('./teste2.txt', {encoding:'utf8', flag:'r'});
-  cabecote = 0
-  console.log(data + "\n")
+  //cabecote = 0
+  //console.log(data + "\n")
   //while(true){
-	for(let i = 0; i < 7; i++){
+	for(let i = 0; i < 12; i++){
     let token = SCANNER(data)
-    if(token?.classeToken == "ERRO"){
-      continue
-    }
-    else{
-      console.log("Classe: " + token?.classeToken + ", Lexema: " + token?.lexemaToken + ", Tipo:" + token?.tipoToken)
-      if(token?.classeToken == "EOF") break
-    }
+    
+    if(token?.classeToken == 'EOF') break
+    if(token?.classeToken == "ERRO") continue
+    else console.log("Classe: " + token?.classeToken + ", Lexema: " + token?.lexemaToken + ", Tipo:" + token?.tipoToken) 
   }
 }
 
